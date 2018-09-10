f61e04c954290695f76dd558f22ecb420699aa41
JC Perez Chavez <jc.perezchavez@gmail.com>
2018-09-09 21:58:40 -0400

    Adding eslint 📐

--- src/themes/index.js
+++ src/themes/index.js
@@ -3,17 +3,17 @@ import standard from './standard';
 const get = (ref, selector, def) => {
   if (typeof ref === 'object' && Object.keys(ref).length) {
     if (ref[selector]) {
-      return ref[selector]
+      return ref[selector];
     }
-    const [token, ...newSelector] = selector.split('.')
-    const newRef = ref[token]
+    const [token, ...newSelector] = selector.split('.');
+    const newRef = ref[token];
     if (typeof newRef === 'object' && Object.keys(newRef).length) {
-      return get(newRef, newSelector.join('.'), def)
+      return get(newRef, newSelector.join('.'), def);
     }
     return def;
   }
   return def;
-}
+};
 
 const theme = {
   bodyBackgroundColor: ({ theme: t = {} } = {}) => t.bodyBackgroundColor,
